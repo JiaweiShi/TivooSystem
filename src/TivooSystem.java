d439ce26f6c94d9b146bff31a13349f0239c188d
Hao Sun <oddsun@gmail.com>
2012-02-15 19:04:05 -0500

    processor added

--- src/TivooSystem.java
+++ src/TivooSystem.java
@@ -22,23 +22,25 @@ public class TivooSystem {
 	
 	
 	public void filterByKeyword(String keyword) {
-		
-		
-		
+		Processor p = new Processor();
+		nodes = p.filterByKeyword(nodes, keyword);
 	}
 	
 	
-	public void filterByTime(String date) {
-		
-		
-		
-		
+	public void filterByTime(String begDate, String endDate) {
+		Processor p = new Processor();
+		nodes = p.filterByDate(nodes, begDate, endDate);
 	}
 	
 	
 	public void outputToHtml(String detailsFile, String summaryFile) {
 		SummaryPageHTMLWriter writer = new SummaryPageHTMLWriter();
 		writer.makeFile(detailsFile, summaryFile, nodes);	
+	}
+	
+	public static void main(String[] args)
+	{
+		
 	}
 	
 }
